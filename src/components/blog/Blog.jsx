53c65bb180aa932899f356bd7d2dbac466247848
nikit34 <permikov134@mail.ru>
2024-08-06 22:37:39 +0100

    Change blog container

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -15,7 +15,7 @@ const Blog = () => {
             <p className="section__subtitle">
                 <Trans i18nKey="blog.subtitle" components={{ s: <span /> }} />
             </p>
-            <div className="blog__container container grid">
+            <div className="blog__container grid">
                 {blogItems.map((item, index) => (
                     <BlogItem
                         key={index}
@@ -26,9 +26,6 @@ const Blog = () => {
                         image={item.image}
                     />
                 ))}
-            </div>
-            <div className='section__deco deco__right'>
-                <img src={shapeOne} alt='' className='shape' />
             </div>
             <div className="section__bg-wrapper">
                 <span className="bg__title">{t('blog.bg_title')}</span>
--- src/components/blog/BlogItem.jsx
+++ src/components/blog/BlogItem.jsx
@@ -3,18 +3,16 @@ import { FaArrowRight } from 'react-icons/fa';
 
 const BlogItem = ({ date, title, description, link, image }) => {
     return (
-        <div className="blog__item card card-two">
-            <span className="blog__date text-cs">{date}</span>
-            <h3 className="blog__title">{title}</h3>
-            <p className="blog__description">{description}</p>
-            <a href={link} className="link">
-                Read More
-                <FaArrowRight className="link__icon" />
-            </a>
-            <div className='blog__img-wrapper'>
-                <img src={image} alt="" className="blog__img" />
+        <a href={link} className="link">
+            <div className="blog__item">
+                <span className="blog__date text-cs">{date}</span>
+                <h3 className="blog__title">{title}</h3>
+                <p className="blog__description">{description}</p>
+                <div className='blog__img-wrapper'>
+                    <img src={image} alt="" className="blog__img" />
+                </div>
             </div>
-        </div>
+        </a>
     );
 };
 
